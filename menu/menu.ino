bd08bb0f8342ebf45541f06ee35f84a9035820dc
Lyubomir Dimitrov <78859431+LuboDimitrov@users.noreply.github.com>
2022-01-20 01:50:29 +0100

    comentarios añadidos

--- menu/menu.ino
+++ menu/menu.ino
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//pines del LCD
 #define PIN_RS A0
 #define PIN_RW A1
 #define PIN_EN A2
@@ -18,7 +19,8 @@ LCD lcd(
 	PIN_DB4, PIN_DB5, PIN_DB6, PIN_DB7
 );
 
-// Keypad
+//pines L: filas
+//pines R: columnas
 #define KEYPAD_COL0_PIN 22
 #define KEYPAD_COL1_PIN 24
 #define KEYPAD_COL2_PIN 26
@@ -46,16 +48,8 @@ char characters[KEYPAD_ROWS*KEYPAD_COLS] = {
 #define ACTION_NEXT   14
 #define ARRAYSIZE 16
 #define STATES 7
-/*
-enum states_t {
-  WIDTH,
-  HEIGHT,
-  DEPTH,
-  SPEED,
-  INTRODUCE, //Introducir nuevo valor
-  MODIFIED   //Parametro modificado correctamente
-};
-*/
+
+//Arrays con los nombres de los estados
 char inicial[ARRAYSIZE] = "Menu";
 char width[ARRAYSIZE] = "Width:";
 char height[ARRAYSIZE] = "Height:";
@@ -64,13 +58,17 @@ char speed[ARRAYSIZE] = "Speed:";
 char introduce[ARRAYSIZE] = "Value:";
 char modified[ARRAYSIZE] = "Modified OK";
 
+//puntero auxiliar que contiene el valor que introduce el usuario
 char* p_aux;
+//punteros que guardarán el valor de p_aux dependiendo del estado actual
 char* p_printWidth;
 char* p_printHeight;
 char* p_printDepth;
 char* p_printSpeed;
+//valor que introduce el usuario en el LCD pasado a long (uso de atol)
 long valueIntroduced;
 
+//punteros a los arrays de estados
 char* p_inicial = inicial;
 char* p_width = width;
 char* p_height = height;
@@ -79,11 +77,10 @@ char* p_speed = speed;
 char* p_introduce = introduce;
 char* p_modified = modified;
 
+//array con los punteros a los estados
 char* states[STATES] = { p_inicial, p_width, p_height, p_depth, p_speed, p_introduce, p_modified };
 
-char buff[16];
-
-
+//Struct con las variables para enviar al servidor
 struct values{
   long width;
   long height;
@@ -93,11 +90,14 @@ struct values{
 
 values currentValues;
 
-char* current_state = states[0];
-uint8_t counter = 0;
+//Variable que contendrá el número del estado actual
+uint8_t current = 0;
+//numero de veces que el usuario ha pulsado OK
 uint8_t oks;
 
 void setup() {
+
+  //INICIALIZACIONES
 
   Serial.begin(9600);
   valueIntroduced = 0;
@@ -108,53 +108,59 @@ void setup() {
 		KEYPAD_ROW0_PIN, KEYPAD_ROW1_PIN, KEYPAD_ROW2_PIN, KEYPAD_ROW3_PIN
 	);
 
-	// Init LCD
 	lcd.init();
-  printState(0);
+  printState(current);
 
 }
 
-void printValue(uint8_t value){
-  lcd.moveCursor(2,1);
-  switch(value){
-    case 1:
-      //lcd.print();
-      break;
-  }
-}
+
+//Método que nos permite imprimir el estado actual por el LCD
+//En el caso de que sea el estado de una variable, también mostramos su valor
 
 void printState(uint8_t state){
   lcd.clear();
   switch(state){
+    //Estado inicial
     case 0:
       lcd.moveCursor(1,7);
       lcd.print(states[0]);
       lcd.moveCursor(2,1);
-      lcd.print("           Sig->");
+      lcd.print("          Next->");
       break;
+    //Estado anchura
     case 1:
       lcd.print(states[1]);lcd.print(p_printWidth);
       lcd.moveCursor(2,1);
-      lcd.print("<-Salir    Sig->");
+      lcd.print("<-Exit    Next->");
       break;
+    //Estado altura
     case 2:
       lcd.print(states[2]);lcd.print(p_printHeight);
       lcd.moveCursor(2,1);
-      lcd.print("<-Ant      Sig->");
+      lcd.print("<-Prev    Next->");
       break;
+    //Estado profundidad
     case 3:
       lcd.print(states[3]);lcd.print(p_printDepth);
       lcd.moveCursor(2,1);
-      lcd.print("<-Ant      Sig->");
+      lcd.print("<-Prev    Next->");
       break;
+    //Estado velocidad
     case 4:
       lcd.print(states[4]);lcd.print(p_printSpeed);
       lcd.moveCursor(2,1);
-      lcd.print("<-Ant");
+      lcd.print("<-Prev          ");
       break;
   }
 }
 
+/*
+Método que nos permite asignar el valor que ha introducido
+el usuario a cada variable del struct currentValues. Además,
+también guardamos en un puntero el valor que recibimos cuando llamamos
+a "lcd.readSecondRow()" para poder imprimirlo en el LCD ya que no podemos
+imprimir variables de tipo long
+*/
 void assignValue(char* pointer, long value, uint8_t state){
 
     switch(state){
@@ -177,31 +183,41 @@ void assignValue(char* pointer, long value, uint8_t state){
     }
 }
 
-
+/*
+Método que nos permite realizar diferentes acciones
+dependiendo de que tecla se ha pulsado
+*/
 
 void action(uint8_t key) {
 	char c = characters[key];
 	if(!c) {
 		switch(key) {
+      //Borra la fila de abajo en el caso de que el usuario
+      //se haya equivocado introduciendo el valor
 			case ACTION_DELETE_ROW:
 				lcd.delete_row(2);
 				break;
+      //Si no queremos introducir ningún valor, volvemos al estado anterior
       case ACTION_CANCEL:
         lcd.clear();
-        counter = counter;
-        printState(counter);
+        current = current;
+        printState(current);
         oks = 0;
         break;
+      //Cambiamos al siguiente estado  
       case ACTION_NEXT:
         lcd.clear();
-        counter = (counter+1) % 5;
-        printState(counter);
+        current = (current+1) % 5;
+        printState(current);
         break;
+      //Volvemos al estado anterior
       case ACTION_PREVIOUS:
         lcd.clear();
-        counter = (counter-1) % 5;
-        printState(counter);
+        current = (current-1) % 5;
+        printState(current);
         break;
+      //Si pulsamos OK 1 vez, nos lleva al estado para introducir un valor
+      //Si pulsamos OK 2 otra vez, confirmamos el valor introducido
       case ACTION_OK:
         oks = oks + 1;
         Serial.println(oks);
@@ -213,13 +229,13 @@ void action(uint8_t key) {
         } else {
           p_aux = lcd.readSecondRow();
           valueIntroduced = atol(p_aux);
-          assignValue(p_aux, valueIntroduced, counter);
+          assignValue(p_aux, valueIntroduced, current);
           delay(500);
           lcd.clear();
           lcd.print(states[6]);
           delay(800);
           lcd.clear();
-          printState(counter);
+          printState(current);
           oks = 0;
         }
 
