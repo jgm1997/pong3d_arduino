d8096ded283d31571f06b970e12304dc3dd2297e
Sergi Vivo Valls <sergivivovalls@gmail.com>
2022-02-01 02:19:36 +0100

    Mostrar estado LCD y errores corregidos

--- Server/Server.ino
+++ Server/Server.ino
@@ -14,6 +14,7 @@
 #include "Pong3D.h"
 #include "Timer.h"
 
+#include "Menu.h"
 
 byte mac[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x04 };
 
@@ -44,6 +45,7 @@ PubSubClient mqttClient(ethClient);
 
 void callback(char *topic, byte *payload, unsigned int length);
 void reconnect();
+void lcdPrintScore();
 
 // Publish topics
 #define TOPIC_BALL_X "/pong3d/ball/x"
@@ -101,9 +103,12 @@ ISR(TIMERV) {
 #define MSG_SIZE 10
 uint8_t* msg;
 
+#define STR_SIZE 50
+char* str;
+
 bool pub_ok;
 bool sub_ok;
-gameevent_t game_event;
+volatile gameevent_t game_event;
 coord_t coords;
 
 /******************************************************************************/
@@ -122,6 +127,7 @@ void setup() {
     
     // Auxiliary variables initialization
     msg = (uint8_t *) malloc(MSG_SIZE);
+    str = (char *) malloc(STR_SIZE);
     response.setAllFalse();
 
     // Timer
@@ -129,8 +135,29 @@ void setup() {
     timer.load();
     
     // Pong3D
-    p3d.setDimensions(500,300,800);
-    p3d.setInitialBallSpeed(8);
+    p3d.setDimensions(
+        currentValues.width, currentValues.height, currentValues.depth
+    );
+    p3d.setInitialBallSpeed(currentValues.speed);
+    
+    // From Menu.cpp
+    valueIntroduced = 0;
+    oks = 0;
+    p_aux = NULL;
+    
+    // Keypad
+	keypad.initKeypad(
+		KEYPAD_COL0_PIN, KEYPAD_COL1_PIN, KEYPAD_COL2_PIN, KEYPAD_COL3_PIN, 
+		KEYPAD_ROW0_PIN, KEYPAD_ROW1_PIN, KEYPAD_ROW2_PIN, KEYPAD_ROW3_PIN
+	);
+
+    // LCD
+	lcd.init();
+    lcd.print(
+        "     Pong3D     "
+        "     Server     "
+    );
+    delay(2000);
 }
 
 
@@ -142,73 +169,109 @@ void loop() {
 
     if (!mqttClient.connected()) reconnect();
     
-    while(p3d.getGameState() == GAME_WAITING) {
+    /**************************************************************************/
+    /** STATE GAME_WAITING ****************************************************/
+    /**************************************************************************/
+    if(p3d.getGameState() == GAME_WAITING) {
+        lcd.clear();
+        lcd.print(
+            "  Waiting for   "
+            "   players...   "
+        );
+        delay(1000);
+    } while(p3d.getGameState() == GAME_WAITING) {
         // Esperar a que se conecten los jugadores (callback)
         mqttClient.loop();
         if(!mqttClient.connected()) break;
+        
+        // Keyboard
+        //key = keypad.readKeypad(BLOCK); // Polling
+	    //action(key);
     }
     
-    while(p3d.getGameState() == GAME_READY) {
+    /**************************************************************************/
+    /** STATE GAME_READY ******************************************************/
+    /**************************************************************************/
+    if(p3d.getGameState() == GAME_READY) {
+        lcd.clear();
+        lcd.print(
+            "   Game ready   "
+            "                "
+        );
+    } while(p3d.getGameState() == GAME_READY) {
         // Esperar a que los jugadores den su visto bueno (callback)
         mqttClient.loop();
         if(!mqttClient.connected()) break;
     }
     
-    while(p3d.getGameState() == GAME_PLAYING) {
+    /**************************************************************************/
+    /** STATE GAME_PLAYING ****************************************************/
+    /**************************************************************************/
+    if(p3d.getGameState() == GAME_PLAYING) {
+        lcd.clear();
+        lcd.print(
+            "     Start!     "
+            "                "
+        );
+        delay(1000);
+        lcdPrintScore();
+    } while(p3d.getGameState() == GAME_PLAYING) {
         // En función del último evento de la actualización anterior
-        switch(game_event) {
-            case NO_EVENT: case BALL_COLLISION:
-                if(timer_flag) {
-                    game_event = p3d.updateGame();
-                    
-                    switch(game_event) {
-                        // Movimiento natural de la pelota
-                        case NO_EVENT: case BALL_COLLISION:
-                            coords = p3d.getBallPosition();
+        if(game_event != BALL_P1_REACH && game_event != BALL_P2_REACH) {
+            if(timer_flag) {
+                game_event = p3d.updateGame();
+                
+                switch(game_event) {
+                    // Movimiento natural de la pelota
+                    case NO_EVENT: case BALL_COLLISION:
+                        coords = p3d.getBallPosition();
 
-                            memcpy(msg, &coords.x, 4);
-                            pub_ok  = mqttClient.publish(TOPIC_BALL_X, msg, 4);
+                        memcpy(msg, &coords.x, 4);
+                        pub_ok  = mqttClient.publish(TOPIC_BALL_X, msg, 4);
 
-                            memcpy(msg, &coords.y, 4);
-                            pub_ok &= mqttClient.publish(TOPIC_BALL_Y, msg, 4);
+                        memcpy(msg, &coords.y, 4);
+                        pub_ok &= mqttClient.publish(TOPIC_BALL_Y, msg, 4);
 
-                            memcpy(msg, &coords.z, 4);
-                            pub_ok &= mqttClient.publish(TOPIC_BALL_Z, msg, 4);
-                            
-                            break;
-                            
-                        // Solicitar coordenadas de la paleta
-                        case BALL_P1_REACH:
-                            pub_ok = mqttClient.publish(TOPIC_P1_REQ, msg, 0);
-                            break;
-                        case BALL_P2_REACH:
-                            pub_ok = mqttClient.publish(TOPIC_P2_REQ, msg, 0);
-                            break;
-                    }
-                    
-                    if (pub_ok) {
-                        Serial.println("All messages Published");
-                    } else {
-                        Serial.println("Some messages couldn't be Published");
-                        if(game_event == BALL_P1_REACH ||
-                                game_event == BALL_P2_REACH) {
-                            p3d.invertVelocityZ(); // Count as hit
-                        }
-                    }
+                        memcpy(msg, &coords.z, 4);
+                        pub_ok &= mqttClient.publish(TOPIC_BALL_Z, msg, 4);
                         
-                    timer_flag = false;
+                        break;
+                        
+                    // Solicitar coordenadas de la paleta
+                    case BALL_P1_REACH:
+                        pub_ok = mqttClient.publish(TOPIC_P1_REQ, msg, 0);
+                        break;
+                    case BALL_P2_REACH:
+                        pub_ok = mqttClient.publish(TOPIC_P2_REQ, msg, 0);
+                        break;
                 }
-                break;
-            case BALL_P1_REACH: case BALL_P2_REACH:
-                // Esperar a recibir la información de las paletas
-                break;
+                
+                if (pub_ok) {
+                    //Serial.println("All messages Published");
+                } else {
+                    Serial.println("Some messages couldn't be Published");
+                    if(game_event == BALL_P1_REACH ||
+                            game_event == BALL_P2_REACH) {
+                        p3d.invertVelocityZ(); // Count as hit
+                    }
+                }
+                    
+                timer_flag = false;
+            }
         }
 
         mqttClient.loop();
         if(!mqttClient.connected()) break;
     }
     
-    while(p3d.getGameState() == GAME_OVER) {
+    /**************************************************************************/
+    /** STATE GAME_OVER *******************************************************/
+    /**************************************************************************/
+    if(p3d.getGameState() == GAME_OVER) {
+        delay(500);
+        lcd.clear();
+        lcdPrintWinner();
+    } while(p3d.getGameState() == GAME_OVER) {
         // Esperar a solicitud para repetir partida (callback)
         mqttClient.loop();
         if(!mqttClient.connected()) break;
@@ -222,9 +285,9 @@ void loop() {
 /******************************************************************************/
 
 void callback(char *topic, byte *payload, unsigned int length) {
-    Serial.print("Message received from [ ");
-    Serial.print(topic);
-    Serial.println(" ]");
+    //Serial.print("Message received from [ ");
+    //Serial.print(topic);
+    //Serial.println(" ]");
     
     // Tokenize topic string
     char* token;
@@ -238,25 +301,38 @@ void callback(char *topic, byte *payload, unsigned int length) {
         
     // Se da por hecho que params[0] == "pong3d"
     
-    if( // Token is /pong3d/connected
+    if( // Topic is /pong3d/connected
         strcmp(params[1], "connected") == 0 && 
         p3d.getGameState() == GAME_WAITING
     ) {
         char* id = (char *) malloc(2);
-        id = p3d.playerConnected() == 0 ? "1" : "2";
+        p3d.playerConnected() == 0 ? strcpy(id, "1") : strcpy(id, "2");
         pub_ok = mqttClient.publish(TOPIC_ASSIGN, id);
-        Serial.println("Jugador conectado");
+        
+        sprintf(str,
+            "   Player %s     "
+            "   connected    ",
+            id
+        );
+        lcd.clear();
+        lcd.print(str);
 
-    } else if( // Token is /pong3d/ready
+    } else if( // Topic is /pong3d/ready
         strcmp(params[1], "ready") == 0 &&
         p3d.getGameState() == GAME_READY
     ) {
         p3d.playerReady();
-        Serial.print("Jugador listo: ");
-        Serial.println((uint8_t) payload[0]);
+        sprintf(str,
+            "   Player %c     "
+            "     ready      ",
+            payload[0]
+        );
+        lcd.clear();
+        lcd.print(str);
 
-    } else if( // Token is /pong3d/+/response
+    } else if( // Topic is /pong3d/+/response
         strcmp(params[2], "response") == 0 &&
+        (game_event == BALL_P1_REACH || game_event == BALL_P2_REACH) &&
         p3d.getGameState() == GAME_PLAYING
     ) {
         // Paddle response after request
@@ -284,14 +360,11 @@ void callback(char *topic, byte *payload, unsigned int length) {
             if(!p3d.computePaddleCollision(
                 response.x, response.y, response.vx, response.vy, paddle)
             ) {
-                Serial.print("Score: ");
-                Serial.print(p3d.getScore1());
-                Serial.print(" / ");
-                Serial.println(p3d.getScore2());
+                lcdPrintScore();
             }
             response.setAllFalse();
             game_event = NO_EVENT;
-     }
+        }
         
     } else if(
         strcmp(params[1], "ready") == 0 &&
@@ -304,11 +377,16 @@ void callback(char *topic, byte *payload, unsigned int length) {
 void reconnect() {
     // Loop until we're reconnected
     while (!mqttClient.connected()) {
-        Serial.println("Attempting MQTT connection ...");
+        lcd.clear();
+        lcd.print("Attempting MQTT connection...");
 
         // Attempt to connect
         if (mqttClient.connect(client_id)) {
-            Serial.println("connected");
+            lcd.clear();
+            lcd.print(
+                "   Connected    "
+                "                "
+            );
             sub_ok = true;
             
             if (mqttClient.subscribe(TOPIC_RESPONSE)) {
@@ -341,11 +419,41 @@ void reconnect() {
                 Serial.println("No se pudo suscribir a todos los tópicos");
                 Serial.println("  Comprueba la sintaxis de las cadenas");
             }
+            
+            delay(1000);
+            lcd.clear();
         } else {
-            Serial.print("failed, rc=");
-            Serial.print(mqttClient.state());
-            Serial.println(" try again in 2 seconds");
+            lcd.clear();
+            sprintf(str, "failed, rc=%d", mqttClient.state());
+            lcd.print(str);
+            lcd.moveCursor(2,1);
+            lcd.print(" try again in 2s");
             delay(2000);
         }
     }
+}
+
+void lcdPrintScore() {
+    lcd.clear();
+    sprintf(str, "Score: %d / %d", p3d.getScore1(), p3d.getScore2());
+    lcd.print(str);
+    
+    lcd.moveCursor(2,1);
+    sprintf(str, " Goal: %d", p3d.getTarget());
+    lcd.print(str);
+
+    lcd.moveCursor(2,17);
+}
+
+void lcdPrintWinner() {
+    lcd.clear();
+    lcd.print("   Game Over!   ");
+    
+    lcd.moveCursor(2,1);
+    uint8_t number = p3d.getScore1() == p3d.getTarget() ? 1 : 2;
+    sprintf(str, " Player %d wins", number);
+    lcd.print(str);
+
+    lcd.moveCursor(2,17);
+   
 }
